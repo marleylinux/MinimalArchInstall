04ff41286096860184a579294ac91c2c9cc8de77
marleylinux <warburtonmarley@proton.me>
2025-03-02 06:07:22 +0000

    Update minimalarchinstall.c

--- minimalarchinstall.c
+++ minimalarchinstall.c
@@ -459,7 +459,7 @@ if (animegirlland) {
     if (next("Do you want to install \033[36mHyprland\033[0m extra packages? (y/n): ") == 'y') {
         printf("Installing \033[36mHyprland\033[0m extra packages\n");
         forward();
-        system("sudo pacman -S --needed --noconfirm nwg-panel nwg-look nwg-bar nwg-drawer hyprpaper hyprlock nwg-hello obs-studio code pavucontrol resources");
+        system("sudo pacman -S --needed --noconfirm nwg-panel nwg-look nwg-bar nwg-drawer hyprpaper hyprlock nwg-hello obs-studio code pavucontrol gnome-disk-utility polkit-gnome nautilus resources");
         forward();
     } else {
         printf("Skipping \033[36mHyprland\033[0m extra packages\n");
